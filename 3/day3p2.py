755ad9d2a68b6f379cc2b32259692950e7229bc8
gaplin <gaplinn@gmail.com>
2024-01-08 21:07:59 +0100

    3

---
+++ 3/day3p2.py
@@ -0,0 +1,34 @@
+import re
+
+input = open('input2.txt').read().splitlines()
+
+claims = []
+for line in input:
+    claims.append([int(x) for x in re.findall(r'[0-9]+', line)])
+
+already_taken_space = set()
+duplicates = set()
+for id, ii, i, width, height in claims:
+    for new_i in range(i, i + height):
+        for new_ii in range(ii, ii + width):
+            position = (new_i, new_ii)
+            if position in already_taken_space:
+                duplicates.add(position)
+            else:
+                already_taken_space.add(position)
+
+result = 0
+for id, ii, i, width, height in claims:
+    for new_i in range(i, i + height):
+        for new_ii in range(ii, ii + width):
+            position = (new_i, new_ii)
+            if position in duplicates:
+                break
+        else:
+            continue
+        break
+    else:
+        result = id
+        break
+
+print(result)
