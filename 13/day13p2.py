5776263931d83280338133f3ceedfe09039c7d51
gaplin <gaplinn@gmail.com>
2024-01-11 17:05:30 +0100

    13

---
+++ 13/day13p2.py
@@ -0,0 +1,88 @@
+from heapq import heappop, heappush
+
+rotations = {
+    '/': lambda x, y: (-y, -x),
+    "\\": lambda x, y: (y, x)
+}
+
+turns = [
+    lambda x, y: (-y, x),
+    lambda x, y: (x, y),
+    lambda x, y: (y, -x)
+]
+
+grid = open('input2.txt').read().splitlines()
+
+directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+n = len(grid)
+m = len(grid[0])
+for i in range(n):
+    grid[i] = [x for x in grid[i]]
+
+carts = [] # [i, ii, turn(0-2), di, dii]
+for i in range(n):
+    for ii in range(m):
+        symbol = grid[i][ii]
+        if symbol in '^>v<':
+            carts.append([i, ii, 0, *directions['^>v<'.index(symbol)]])
+            if symbol in '><':
+                grid[i][ii] = '-'
+            else:
+                grid[i][ii] = '|'
+
+moves_Q = []
+for idx, cart in enumerate(carts):
+    heappush(moves_Q, (cart[0], cart[1], idx))
+
+def move(cart: list, grid: list, rotations: dict, turns: list) -> None:
+    i, ii, turn_count, di, dii = cart
+    symbol = grid[i][ii]
+    if symbol in '|-':
+        cart[0] += di
+        cart[1] += dii
+        return
+    
+    if symbol in '/\\':
+        new_direction = rotations[symbol](di, dii)
+    else:
+        turn = turns[turn_count]
+        turn_count = (turn_count + 1) % 3
+        new_direction = turn(di, dii)
+        cart[2] = turn_count
+
+    cart[0] += new_direction[0]
+    cart[1] += new_direction[1]
+    cart[3], cart[4] = new_direction[0], new_direction[1]
+
+def collision(carts: dict, i: int, ii: int) -> list:
+    collisions = []
+    for idx, cart in carts.items():
+        if cart[0] == i and cart[1] == ii:
+            collisions.append(idx)
+            
+    return collisions
+
+result = ()
+carts = {idx: cart for idx, cart in enumerate(carts)}
+while True:
+    new_moves_Q = []
+    while len(moves_Q) > 0:
+        _, _, cart_id = heappop(moves_Q)
+        if cart_id not in carts:
+            continue
+
+        cart = carts[cart_id]
+        move(cart, grid, rotations, turns)
+        collisions = collision(carts, cart[0], cart[1])
+        if len(collisions) > 1:
+            for id in collisions:
+                del carts[id]
+        else:
+            heappush(new_moves_Q, (cart[0], cart[1], cart_id))
+
+    moves_Q = new_moves_Q
+    if len(carts) == 1:
+        break
+
+idx, cart = carts.popitem()
+print('{},{}'.format(cart[1], cart[0]))
